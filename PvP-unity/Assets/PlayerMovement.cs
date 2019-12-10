cc4d144543e981423ab8315f175a3b0386fd5dac
ALooter <58535072+ALooter@users.noreply.github.com>
2019-12-10 07:10:05 +0200

    Prototype 0.5
    Created level prototype, basic player movement

---
+++ PvP-unity/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float moveSpeed = 12f;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = transform.right * x + transform.forward * z;
+
+        controller.Move(movementVector * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ PvP-unity/Assets/PlayerRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    public Transform player;
+
+    public float mouseSens = 100f;
+    public float maxCameraAngle = 60f;
+
+    float xRotation;
+
+    void Start()
+    {
+        //lock cursor in center and hide it
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseXpos = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSens;
+        float mouseYpos = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSens;
+
+        xRotation -= mouseYpos;
+        xRotation = Mathf.Clamp(xRotation, -maxCameraAngle, maxCameraAngle);
+
+        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseXpos);
+    }
+}
